ae587487431d90f4c474c419f1180118a9e2c2b5
kumarjai1 <jaiessarani@gmail.com>
2020-04-18 15:54:31 -0400

    policy iteration value updated

--- PolicyIteration/policy_iteration.py
+++ PolicyIteration/policy_iteration.py
@@ -1,6 +1,8 @@
 import math
+import copy
 from pprint import pprint
 from mdp.gridworld.grid_world_mdp import GridWorldMDP
+
 
 class PolicyIteration():
     ''' 
@@ -17,11 +19,26 @@ class PolicyIteration():
     def update_value_function(self):
         pass
 
+    def one_step_lookahead(self, state):
+        updated_value = 0
+        for action in self.mdp.actions:
+            new_state = self.mdp.actions[action](state)
+            value_of_new_state = self.mdp.value[new_state[0]][new_state[1]]
+            updated_value += self.mdp.policy[state][action] * \
+                (self.mdp.reward(new_state, action)+value_of_new_state)
+
+        return updated_value
+
     def evaluate_policy(self):
         current_policy = self.mdp.policy
-        current_value_function = self.mdp.value_function
+        current_value_function = copy.deepcopy(self.mdp.value)
         for state in self.mdp.states:
-            pprint(state)
+            if(state in self.mdp.terminal_states):
+                continue
+            new_value = self.one_step_lookahead(state)
+            current_value_function[state[0]][state[1]] = new_value
+        pprint(current_value_function)
+        return current_value_function
 
     def improve_policy(self):
         pass
@@ -36,4 +53,11 @@ class PolicyIteration():
 
 if __name__ == "__main__":
     mdp = GridWorldMDP()
-    policy = PolicyIteration(mdp).evaluate_policy()
+    for i in range(0, 10):
+        new_value_function = PolicyIteration(mdp).evaluate_policy()
+        mdp.value = new_value_function
+
+    # mdp.value = new_value_function
+    # new_value_function = PolicyIteration(mdp).evaluate_policy()
+    # mdp.value = new_value_function
+    # new_value_function = PolicyIteration(mdp).evaluate_policy()
