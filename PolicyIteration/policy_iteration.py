eb5b82c24baf550f43b521bdbe3d927859ea5315
kumarjai1 <jaiessarani@gmail.com>
2020-04-26 11:43:25 -0400

    wip improve policy

--- PolicyIteration/policy_iteration.py
+++ PolicyIteration/policy_iteration.py
@@ -1,20 +1,20 @@
+from mdp.gridworld.grid_world_mdp import GridWorldMDP
 import math
 import copy
 from pprint import pprint
 import numpy as np
 np.set_printoptions(precision=2)
-from mdp.gridworld.grid_world_mdp import GridWorldMDP
 
 
 class PolicyIteration():
-    ''' 
+    '''
         Implementation of Policy Iteration Reinforcement Learning Algorithm.
         Used for Planning Problems - when full dynamics of a system are known.
         @author ashish juneja
-        @author jai kumar 
+        @author jai kumar
     '''
 
-    def __init__(self, mdp,max_iter,bellman_tolerance):
+    def __init__(self, mdp, max_iter, bellman_tolerance):
         self.mdp = mdp
         self.max_iter = max_iter
         self.bellman_tolerance = bellman_tolerance
@@ -32,7 +32,8 @@ class PolicyIteration():
                 '''Compute the value of the new state by using the Bellman Expectation Equation'''
                 value_of_new_state = self.mdp.value[new_state[0]][new_state[1]]
                 updated_value += self.mdp.policy[state][action] * \
-                    self.mdp.transition_prbability(state,action,new_state)*(self.mdp.reward(new_state, action)+value_of_new_state)
+                    self.mdp.transition_prbability(
+                        state, action, new_state)*(self.mdp.reward(new_state, action)+value_of_new_state)
 
         return updated_value
 
@@ -46,8 +47,36 @@ class PolicyIteration():
             current_value_function[state[0]][state[1]] = new_value
         return current_value_function
 
-    def improve_policy(self,new_value_function):
-        return {}
+    def improve_policy(self, new_value_function):
+
+        new_policy = {}
+        for state in self.mdp.states:
+            if(state in self.mdp.terminal_states):
+                continue
+            q = 0
+            new_action_qs = {}
+
+            for action in self.mdp.actions:
+                new_states = self.mdp.actions[action](state)
+                pprint(new_states)
+
+                for state_prime in new_states:
+                    q += new_value_function[state_prime[0]
+                                            ][state_prime[1]] * self.mdp.transition_prbability(state, action, state_prime)
+                    new_action_qs[action] = q
+
+            pprint(new_action_qs)
+            max_q = max(new_action_qs, key=new_action_qs.get)
+            new_action_qs = {k: v for k,
+                             v in new_action_qs.items() if v == new_action_qs[max_q]}
+
+            for k in new_action_qs.keys():
+                new_action_qs[k] = 1 / len(new_action_qs)
+            new_policy[state] = new_action_qs
+        pprint(new_policy)
+        exit()
+
+        return new_action_qs.keys()
 
     def find_optimal_policy(self):
         new_value_function = self.evaluate_policy()
@@ -57,16 +86,19 @@ class PolicyIteration():
         for i in range(0, self.max_iter):
             old_value_function = mdp.value
             new_value_function = self.update_value_function()
-            bellman_error = abs(np.max((np.subtract(old_value_function,new_value_function))))
+            bellman_error = abs(
+                np.max((np.subtract(old_value_function, new_value_function))))
             mdp.value = new_value_function
-            
-            print("MDP Value Function After Iteration {}:\n {}".format(i,mdp))
+
+            print("MDP Value Function After Iteration {}:\n {}".format(i, mdp))
             if(bellman_error < self.bellman_tolerance):
                 print("exiting early, bellman error is below tolerance")
                 break
-        
+
         return new_value_function
+
 
 if __name__ == "__main__":
     mdp = GridWorldMDP()
-    PolicyIteration(mdp,max_iter=100,bellman_tolerance=0.01).find_optimal_policy()
+    PolicyIteration(mdp, max_iter=100,
+                    bellman_tolerance=0.01).find_optimal_policy()
